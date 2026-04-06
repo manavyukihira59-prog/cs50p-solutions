484194d15b7271f6982fddd0c247cf7dd962e525
manav_ <manavyukihira59@gmail.com>
2026-04-06 12:28:00 +0530

    Add files via upload

---
+++ week0/week0_sol4.py
@@ -0,0 +1,4 @@
+c=3* (10**8)
+print("E=mc^2 is an expression given by Albert Einstein which helps in calculating Energy based on value of mass and speed of light\n")
+m=int(input("Enter value of mass\n"))
+print("Value of Energy according to above expression will be E=", (m*(c**2)) )
