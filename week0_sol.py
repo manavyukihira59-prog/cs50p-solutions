23827b10d7ca77add478f8d559abd786c435e527
manav_ <manavyukihira59@gmail.com>
2026-04-06 11:29:34 +0530

    week0_sol.py

--- week0_sol.py
+++ week0_sol.py
@@ -1,9 +1,9 @@
 def low(y):
     y=y.lower()
-    print(y)
+    return y
     
 
 
 x=input("Marc what are you doing?\n")
-print("Speak in lower tone or your father wil get angry")
-low(x)
+print("Speak in lower tone or your father will get angry")
+print(low(x))
