68019077fc07b800df848fa4b9dbe7131b42cdd3
manav_ <manavyukihira59@gmail.com>
2026-04-06 11:45:53 +0530

    Delete week0_sol.py

--- week0_sol.py
+++
@@ -1,9 +0,0 @@
-def low(text):
-    text=text.lower()
-    return text
-    
-
-
-u_input=input("Marc what are you doing?\n")
-print("Speak in lower tone or your father will get angry")
-print(low(u_input))
