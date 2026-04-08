3dd6e1257b0889be0e528efbc26b0d995551bfea
manav_ <manavyukihira59@gmail.com>
2026-04-08 11:01:07 +0530

    Add files via upload

---
+++ week1/week1_sol2.py
@@ -0,0 +1,9 @@
+interact = input("Greetings : ")
+z= interact.strip()[0]
+
+if "Hello" or "hello" in interact:
+    print("$0")
+elif z=="h" or z=="H":
+    print("$20")
+else:
+    print("$100")        
